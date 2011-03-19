f5164e2f72d053b6bc1345be6d57004b5eff7dad
ed veiga <ed.cinemascop@yahoo.es>
2011-03-18 23:15:26 -0300

    various fixes

--- login_form.php
+++ login_form.php
@@ -1,17 +1,3 @@
-<head><style>#login{
-	width:300px;
-	margin:auto;
-}
-#login_form{
-	Font-family:inherit;
-	
-}
-input{
-	width:280px;
-	height:25px;
-	border-style:none;
-	Font-size:22px;
-}</style></head>
 <div id='login'>
 	<form id='login_form' method='post'>
 		<input class='login_text' type='text' name='user'>
